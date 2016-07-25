5529f0b2edd9e12cfb60913c58f7cfaa3cbad600
Pablo Terradillos <tehsis@gmail.com>
2016-07-24 22:47:40 -0300

    changed init method

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -8,11 +8,12 @@ interface IGameConstructorArgs {
 
 class Game {
     states: {}
+    args: IGameConstructorArgs
     
     constructor (args: IGameConstructorArgs) {
         this.game = new Phaser.Game(args.width, args.height, Phaser.AUTO, args.element);
         this.states = args.states;
-        this.loadStates(args.default_state || 'Boot');
+        this.args = args;
     }
    
     game: Phaser.Game
@@ -21,6 +22,10 @@ class Game {
         Object.keys(this.states).forEach((state_name) => this.game.state.add(state_name, this.states[state_name]));
         
         this.game.state.start(default_state)
+    }
+
+    init () {
+       this.loadStates(this.args.default_state || 'Boot');
     }
 }
 
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,11 +5,13 @@ import * as states from './states/index';
 class BunnyWars extends Game {}
 
 window.onload = () => {
-  new BunnyWars({
+  const bunnywars = new BunnyWars({
       width: GAME.SCREEN.BASE_WIDTH,
       height: GAME.SCREEN.BASE_HEIGHT,
       element: GAME.DOM_ELEMENT,
       states: states,
       default_state: GAME.DEFAULT_STATE
   });
+
+  bunnywars.init();
 };
